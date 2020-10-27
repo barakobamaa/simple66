d0448cad120547101b5327b3fe798eb68b202cf6
barakobamaa <qanbariali95@gmail.com>
2020-10-28 03:00:35 +0330

    dddui

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ ALLOWED_EXTENSIONS = { 'jpg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
